10d5dc41ac237f2d39a307e7d8bcbd69a6a20177
Alaine Miller <alaine.miller@bbc.co.uk>
2021-05-07 17:14:25 +0100

    adding FizzBuzz Solution

---
+++ FizzBuzz/src/dev/alaine/Main.java
@@ -0,0 +1,21 @@
+package dev.alaine;
+
+public class Main {
+
+    public static void main(String[] num) {
+        for (int i = 0; i < 16; i++) {
+            if (i % 3 == 0 && i % 5 ==0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
