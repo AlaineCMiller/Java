e1f988a49034d8eccf8354c324be13f9cf729e24
Alaine Miller <44164856+AlaineCMiller@users.noreply.github.com>
2021-05-07 17:17:30 +0100

    Update Main.java

--- FizzBuzz/src/dev/alaine/Main.java
+++ FizzBuzz/src/dev/alaine/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] num) {
         for (int i = 0; i < 16; i++) {
-            if (i % 3 == 0 && i % 5 ==0) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             }
             else if (i % 3 == 0){
